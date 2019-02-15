21cc46d724ab0b6f46eed6f1cc8aae497ac4e0a4
Jere Sanisalo <XMunkki@users.noreply.github.com>
2019-02-15 18:59:05 +0200

    Ported GLSL compatibility and iq:s basic raymarching example.

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     public Material RenderMaterial;
+    private float m_time = 0.0f;
+    private int m_frame_count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,8 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        m_time += Time.deltaTime;
+        ++m_frame_count;
     }
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
@@ -26,8 +30,10 @@ public class Game : MonoBehaviour
         int xres = 320;
         int yres = (xres * Screen.height) / Screen.width;
 
+        RenderMaterial.SetFloat("_MyTime", m_time);
         RenderMaterial.SetFloat("_ResX", xres);
         RenderMaterial.SetFloat("_ResY", yres);
+        RenderMaterial.SetInt("_Frame", m_frame_count);
 
         RenderTexture scaled = RenderTexture.GetTemporary(xres, yres);
         Graphics.Blit(source, scaled, RenderMaterial);
