f47c53fb10bd21d3a3086ab0dccfcbc7046c162d
Antti Svenn <antti.svenn@gmail.com>
2019-02-16 19:59:19 +0200

    Inputs...

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Rewired;
+
+public class InputHandler : MonoBehaviour {
+    public int playerId = 0;
+
+    public bool FlipPressed;
+    public bool PlugPressed;
+    public bool EscPressed;
+    private Player player;
+
+    void Awake() {
+        Debug.Log("ASDASD: InputHandler::Awake");
+        player = ReInput.players.GetPlayer(playerId);
+        Debug.Log(player);
+    }
+
+    void Update () {
+        GetInput();
+    }
+
+    private void GetInput() {
+        FlipPressed = player.GetButton("Flip");
+        PlugPressed = player.GetButton("Plug");
+        EscPressed = player.GetButton("Quit");
+    }
+
+}
